a3cd060a88f991af5d29b0359a06bd31cf3cb988
heyanliang <heyanliang@bus365.com>
2017-04-20 20:19:42 +0800

    已修改        model/searcha.model.php 搜索优化

--- model/searcha.model.php
+++ model/searcha.model.php
@@ -1,27 +1,27 @@
 <?php
-function jiequ($data,$num=56){
-	if(mb_strwidth($data,'utf-8')>=$num){
-		return mb_strimwidth($data, 0, $num-3,'...','utf-8');
-	}else{
-		return $data;
-	}
-	
+function jiequ($data,$num=55){
+    if(mb_strwidth($data,'utf-8')>=$num){
+        return mb_strimwidth($data, 0, $num-1,'...','utf-8');
+    }else{
+        return $data;
+    }
+
 }
 $s_search['keyWord'] = req('value');
 $s_liebiao = json_decode($db->api_post($host."/travel/interface/zby/getZbyGoodsListFromMongo",$s_search),true);
 $s_liebiao = $s_liebiao['data']['zbyGoodsList'];
 if(!empty($s_liebiao)){
-	echo '<ul>';
-	foreach($s_liebiao as $key=>$value){
-		if($key<7){
-			$goodsName = str_replace(' ','&nbsp;',$value['goodsName']) ;
-			$jiequ = jiequ($value['goodsName']);
-			echo '<li title='.utf8_to_gbk($goodsName).'>'.utf8_to_gbk($jiequ).'</li>';
-//			echo '<li title='.$goodsName.'>'.$jiequ.'</li>';
-		}
-	
-	} 
-echo '</ul>'; 
+    echo '<ul>';
+    foreach($s_liebiao as $key=>$value){
+        if($key<7){
+            $goodsName = str_replace(' ','&nbsp;',$value['goodsName']) ;
+            $jiequ = jiequ($value['goodsName']);
+            echo '<li title='.utf8_to_gbk($goodsName).'>'.utf8_to_gbk($jiequ).'</li>';
+//            echo '<li title='.$goodsName.'>'.$jiequ.'</li>';
+        }
+
+    }
+    echo '</ul>';
 }
 
 ?>
