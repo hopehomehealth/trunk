30e9871fa9b3bc4319cd643ec171d535b8e1c5ff
zhangweiyao <zhangweiyao@bus365.com>
2017-04-20 17:33:07 +0800

    已修改        model/product_detail.model.php

--- model/product_detail.model.php
+++ model/product_detail.model.php
@@ -158,7 +158,7 @@ $all_tools		= $all_days['tool'];
 $all_images		= $all_days['image'];
 
 $goodsId = req('id');
-$url =  "192.168.0.223/travel/interface/zbyV3.2/getZbyGoodsDtailV_3.2?goodsId=" . $goodsId;
+$url = $host . "/travel/interface/zbyV3.2/getZbyGoodsDtailV_3.2?goodsId=" . $goodsId;
 $rst = $db->api_post($url);
 $arr = json_decode($rst, true);
 $data = $arr['data'];
