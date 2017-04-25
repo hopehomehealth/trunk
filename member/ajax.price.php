f7e5a949d990601ac0c6b16c0b5ebf41c036c62d
zhangweiyao <zhangweiyao@bus365.com>
2017-04-25 09:30:56 +0800

    已修改        member/ajax.price.php

--- member/ajax.price.php
+++ member/ajax.price.php
@@ -17,5 +17,12 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 $data = curl_exec($ch);
 curl_close($ch);
 $arr = json_decode($data, true);
-$datas = $arr['data'];
+$fangcha = $arr['data'];
 
+echo "<span>¹ºÂò·¿²î¼Û</span>
+                    <select>";
+foreach ($fangcha as $key => $val) {
+    echo "<option value='$val'>$val</option>";
+}
+echo "</select>
+                    <span class=\"jiage\">+ &yen;80</span>";
