900c655091d7d58eb2ee99facda0af6645fcd2ef
wanghaonan <wanghaonan@bus365.com>
2017-04-25 15:39:35 +0800

    已修改        themes/s01/zbyform_submit.php
    Signed-off-by: wanghaonan <wanghaonan@bus365.com>

--- themes/s01/zbyform_submit.php
+++ themes/s01/zbyform_submit.php
@@ -26,7 +26,7 @@ if(!defined('IN_CLOOTA')) {
             <img src="/themes/s01/images/zby_fillInOrder.jpg">
         </div>
         <form name="write_form" id="write_form" method="post" action="/zhoubianyou/zbyform_submit-<?=$goods_type;?>-<?=$goods_id;?>-<?=$goods_name;?>-<?=$adult_num;?>-<?=$kid_num;?>-<?=$departdate;?>-<?=$pay_price;?>.html?flag=check">
-        <? if($_POST['isPackage'] == 'false'){ ?>
+        <? if($is_package == 'false'){ ?>
             <div class="zbyOrder_main1">
 
                 <div class="zbyOrder_main1_title">
@@ -136,68 +136,164 @@ if(!defined('IN_CLOOTA')) {
                 <div class="zbyOrder_main2_title">联系人信息</div>
 
                 <div class="zbyOrder_main2_buy">
-                    <div class="zbyOrder_main2_buyLeft">购买人</div>
+                    <div class="zbyOrder_main2_buyLeft">联系人</div>
                     <div class="zbyOrder_main2_buyRight">
                         <ul>
                             <li>
                                 <label><b>＊</b>姓名：</label>
-                                <input type="text" name="linker" id="linker" value="">
+                                <input type="text" name="bookerName" id="linker" value="">
                                 <span class="buyer_nameTips">购买人姓名不能为空！</span>
                             </li>
                             <li>
                                 <label><b>＊</b>手机号码：</label>
-                                <input type="text" name="mobile" id="mobile" value="">
+                                <input type="text" name="bookerMobile" id="mobile" value="">
                                 <span>此手机为接受短信所用，作为订购于取票凭证，请准确填写。</span>
                                 <span class="buyer_phoneTips">购买人手机号不能为空！</span>
+                            </li>
+                            <li>
+                                <label><b>＊</b>邮箱：</label>
+                                <input type="text" name="bookerEmail" id="email" value="">
+                                <span class="buyer_emailTips"></span>
+                            </li>
+                            <li>
+                                <label><b>＊</b>用户类型：</label>
+                                <select style="width: 138px;" name="userType">
+                                    <option value="0">bus365用户</option>
+                                    <option value="1">企业用户</option>
+                                </select>
                             </li>
                         </ul>
                     </div>
                 </div>
-
+                <? for($i=0;$i<$num;$i++){ ?>
                 <div class="zbyOrder_main2_youwan">
                     <div class="zbyOrder_main2_youwanLeft">游玩人1</div>
                     <div class="zbyOrder_main2_youwanRight">
                         <ul>
                             <li>
                                 <label><b>＊</b>姓名：</label>
-                                <input type="text" name="userName" autocomplete="off" id="youwan_userName">
-                                <span class="youwan_nameTips">游玩人姓名不能为空！</span>
+                                <input type="text" name="name_<?=$i?>" autocomplete="off" id="youwan_userName_<?=$i?>">
+                                <span class="youwan_nameTips_<?=$i?>"></span>
                             </li>
                             <li>
                                 <label><b>＊</b>手机号码：</label>
-                                <input type="text" name="userPhone" autocomplete="off" id="youwan_userPhone">
-                                <span class="youwan_phoneTips">游玩人手机号不能为空！</span>
+                                <input type="text" name="mobile_<?=$i?>" autocomplete="off" id="youwan_userPhone_<?=$i?>">
+                                <span class="youwan_phoneTips_<?=$i?>"></span>
+                            </li>
+                            <li>
+                                <label><b>＊</b>邮箱：</label>
+                                <input type="text" name="email_<?=$i?>" id="youwan_email_<?=$i?>" value="">
+                                <span class="youwan_emailTips_<?=$i?>"></span>
+                            </li>
+                            <li>
+                                <label><b>＊</b>英文名</label>
+                                <input type="text" name="enName_<?=$i?>" autocomplete="off" id="youwan_enName_<?=$i?>">
+                                <span class="youwan_enNameTips_<?=$i?>"></span>
                             </li>
                             <li>
                                     <label><b>＊</b>人群：</label>
-                                    <select name="userType">
+                                    <select name="personType_<?=$i?>">
                                         <option value="adult">成人</option>
                                         <option value="child">儿童</option>
                                     </select>
+                                    <label><b>＊</b>性别：</label>
+                                    <select name="gender_<?=$i?>">
+                                        <option value="male">男</option>
+                                        <option value="chifemaleld">女</option>
+                                    </select>
+
                             </li>
                             <li>
                                 <label><b>＊</b>证件类型：</label>
-                                <select style="width: 138px;" name="userIdcard">
+                                <select style="width: 138px;">
                                     <option>身份证</option>
                                 </select>
-                                <input type="text" name="userIdcard" autocomplete="off"  id="youwan_userIdcard">
-                                <span class="youwan_idTips">游玩人证件号不能为空！</span>
+                                <input type="text" name="credentials_<?=$i?>" autocomplete="off"  id="youwan_userIdcard_<?=$i?>">
+                                <span class="youwan_idTips_<?=$i?>"></span>
+                            </li>
+                            <li>
+                                <label><b>＊</b>生日</label>
+                                <input type="date" name="birthday_<?=$i?>" autocomplete="off" id="youwan_userName"  min="1900-09-16" max="<?echo date("Y-m-d",time());?>"></span>
                             </li>
                         </ul>
                     </div>
+                    <script type="text/javascript">
+                    $('#youwan_userName_<?=$i?>').blur(function(){
+                        if($('#youwan_userName_<?=$i?>').val()==''){
+                            $('.youwan_nameTips_<?=$i?>').show().html('游玩人姓名不能为空').css('color','red');
+                            youwanName_flag_<?=$i?>=0;
+                        }else{
+                            $('.youwan_nameTips_<?=$i?>').hide();
+                            youwanName_flag_<?=$i?>=1;
+                        }
+                    });
+                    $('#youwan_userPhone_<?=$i?>').blur(function(){
+                        if($('#youwan_userPhone_<?=$i?>').val()==''){
+                            $('.youwan_phoneTips_<?=$i?>').show().html('游玩人手机号不能为空').css('color','red');
+                        }else if(reg2.test($('#youwan_userPhone_<?=$i?>').val())){
+                            $('.youwan_phoneTips_<?=$i?>').show();
+                            $('.youwan_phoneTips_<?=$i?>').html('');
+                            youwanPhone_flag_<?=$i?>=1;
+                        }else if(!reg2.test($('#youwan_userPhone_<?=$i?>').val())){
+                            $('.youwan_phoneTips_<?=$i?>').show();
+                            $('.youwan_phoneTips_<?=$i?>').html('请输入正确的手机号！').css('color','red');
+                            youwanPhone_flag_<?=$i?>=0;
+                        }
+                    });
+                    $('#youwan_enName_<?=$i?>').blur(function(){
+                        if($('#youwan_enName_<?=$i?>').val()==''){
+                            $('.youwan_enNameTips_<?=$i?>').show().html('游玩人英文名不能为空').css('color','red');
+                        }else if(reg5.test($('#youwan_enName_<?=$i?>').val())){
+                            $('.youwan_enNameTips_<?=$i?>').show();
+                            $('.youwan_enNameTips_<?=$i?>').html('');
+                            youwanenName_flag_<?=$i?>=1;
+                        }else if(!reg5.test($('#youwan_enName_<?=$i?>').val())){
+                            $('.youwan_enNameTips_<?=$i?>').show();
+                            $('.youwan_enNameTips_<?=$i?>').html('请输入正确的英文名！').css('color','red');
+                            youwanenName_flag_<?=$i?>=0;
+                        }
+                    });
+                    //邮箱
+                    $('#youwan_email_<?=$i?>').blur(function(){
+                        if($('#youwan_email_<?=$i?>').val()==''){
+                            $('.youwan_emailTips_<?=$i?>').show().html('游玩人邮箱不能为空').css('color','red');
+                        }else if(reg4.test($('#youwan_email_<?=$i?>').val())){
+                            $('.youwan_emailTips_<?=$i?>').show();
+                            $('.youwan_emailTips_<?=$i?>').html('');
+                            youwanEmail_flag_<?=$i?>=1;
+                        }else if(!reg4.test($('#youwan_email_<?=$i?>').val())){
+                            $('.youwan_emailTips_<?=$i?>').show();
+                            $('.youwan_emailTips_<?=$i?>').html('请输入正确的邮箱！').css('color','red');
+                            youwanEmail_flag_<?=$i?>=0;
+                        }
+                    });
+                    $('#youwan_userIdcard_<?=$i?>').blur(function(){
+                        if($('#youwan_userIdcard_<?=$i?>').val()==''){
+                            $('.an_idTipsyouw_<?=$i?>').show().html('游玩人证件号不能为空').css('color','red');
+                        }else if(reg3.test($('#youwan_userIdcard_<?=$i?>').val())){
+                            $('.youwan_idTips_<?=$i?>').show();
+                            $('.youwan_idTips_<?=$i?>').html('');
+                            youwanIdNum_flag_<?=$i?>=1;
+                        }else if(!reg3.test($('#youwan_userIdcard_<?=$i?>').val())){
+                            $('.youwan_idTips_<?=$i?>').show();
+                            $('.youwan_idTips_<?=$i?>').html('请输入正确的证件号！').css('color','red');
+                            youwanIdNum_flag_<?=$i?>=0;
+                        }
+                    });
+                    </script>
                 </div>
-                
+                <?}?>
                 <div class="zbyOrder_main2_youwan">
                     <div class="zbyOrder_main2_youwanLeft">紧急联系人</div>
                     <div class="zbyOrder_main2_youwanRight">
                         <ul>
                            <li>
                                <label><b>＊</b>姓名：</label>
-                                <input type="text" name="userName" autocomplete="off">
+                                <input type="text" name="emergencyName" autocomplete="off">
                             </li>
                             <li>
                                 <label><b>＊</b>手机号码：</label>
-                                <input type="text" name="userPhone" autocomplete="off">
+                                <input type="text" name="emergencyMobile" autocomplete="off">
                                 <span></span>
                             </li>
                             
@@ -205,7 +301,15 @@ if(!defined('IN_CLOOTA')) {
                     </div>
                 </div>
             </div>
-        <input type="hidden" name="" value="">
+        <input type="hidden" name="goodsId" value="">
+        <input type="hidden" name="lvProductId" value="">
+        <input type="hidden" name="packageId" value="">
+        <input type="hidden" name="departdate" value="">
+        <input type="hidden" name="payPrice" value="">
+        <input type="hidden" name="packageNum" value="">
+        <input type="hidden" name="adultNum" value="">
+        <input type="hidden" name="childNum" value="">
+        <input type="hidden" name="roomCount" value="">
         </form>
 
         <?
@@ -254,7 +358,7 @@ if(!defined('IN_CLOOTA')) {
 <script type="text/javascript" src="/themes/s01/js/jquery.js"></script>
 <script type="text/javascript" src="/themes/s01/js/common.js"></script>
 <script type="text/javascript">
-$(document).ready(function(){
+/*$(document).ready(function(){
 
 	var adds = $('.add');
 	for (var i = 0; i < adds.length; i++) {
@@ -279,23 +383,41 @@ $(document).ready(function(){
 	}
 
 
-});
+});*/
 
 //表单合发性验证
 var reg1 = /^([\u4e00-\u9fa5]){2,6}$/;//匹配中文
 var reg2 = /^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$/;//匹配手机号
 var reg3 = /(^\d{15}$)|(^\d{17}(\d|X)$)/;//匹配身份证
+var reg4 = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;//匹配邮箱
+var reg5 = /^[A-Za-z]+$/;//英文名
 var buyerName_flag=0,
     buyerPhone_flag=0,
+    buyerEmail_flag=0,
     // phoneNum_flag=0,
     // idNum_flag=0,
     // email_flag=0,
     // tiaokuan_flag=1
     youwanName_flag=0,
     youwanPhone_flag=0,
-    youwanIdNum_flag=0
+    youwanenName_flag=0,
+    youwanIdNum_flag=0,
+    buyerEmail_flag=0
     ;
-
+//邮箱
+$('#email').blur(function(){
+    if($('#email').val()==''){
+        $('.buyer_emailTips').show().html('联系人邮箱不能为空').css('color','red');
+    }else if(reg4.test($('#email').val())){
+        $('.buyer_emailTips').show();
+        $('.buyer_emailTips').html('');
+        buyerEmail_flag=1;
+    }else if(!reg4.test($('#email').val())){
+        $('.buyer_emailTips').show();
+        $('.buyer_emailTips').html('请输入正确的邮箱！').css('color','red');
+        buyerEmail_flag=0;
+    }
+});
 
 $('#linker').blur(function(){
     if($('#linker').val()==''){
@@ -308,53 +430,19 @@ $('#linker').blur(function(){
 });
 $('#mobile').blur(function(){
     if($('#mobile').val()==''){
-        $('.buyer_phoneTips').show().html('购买人手机号不能为空');
+        $('.buyer_phoneTips').show().html('联系人手机号不能为空').css('color','red');
     }else if(reg2.test($('#mobile').val())){
         $('.buyer_phoneTips').show();
         $('.buyer_phoneTips').html('');
         buyerPhone_flag=1;
     }else if(!reg2.test($('#mobile').val())){
         $('.buyer_phoneTips').show();
-        $('.buyer_phoneTips').html('请输入正确的手机号！');
+        $('.buyer_phoneTips').html('请输入正确的手机号！').css('color','red');
         buyerPhone_flag=0;
     }
 });
 
-$('#youwan_userName').blur(function(){
-    if($('#youwan_userName').val()==''){
-        $('.youwan_nameTips').show();
-        youwanName_flag=0;
-    }else{
-        $('.youwan_nameTips').hide();
-        youwanName_flag=1;
-    }
-});
-$('#youwan_userPhone').blur(function(){
-    if($('#youwan_userPhone').val()==''){
-        $('.youwan_phoneTips').show().html('游玩人手机号不能为空');
-    }else if(reg2.test($('#youwan_userPhone').val())){
-        $('.youwan_phoneTips').show();
-        $('.youwan_phoneTips').html('');
-        youwanPhone_flag=1;
-    }else if(!reg2.test($('#youwan_userPhone').val())){
-        $('.youwan_phoneTips').show();
-        $('.youwan_phoneTips').html('请输入正确的手机号！');
-        youwanPhone_flag=0;
-    }
-});
-$('#youwan_userIdcard').blur(function(){
-    if($('#youwan_userIdcard').val()==''){
-        $('.youwan_idTips').show().html('游玩人证件号不能为空');
-    }else if(reg3.test($('#youwan_userIdcard').val())){
-        $('.youwan_idTips').show();
-        $('.youwan_idTips').html('');
-        youwanIdNum_flag=1;
-    }else if(!reg3.test($('#youwan_userIdcard').val())){
-        $('.youwan_idTips').show();
-        $('.youwan_idTips').html('请输入正确的证件号！');
-        youwanIdNum_flag=0;
-    }
-});
+
 
 function check_form(){
     if($('.zbyOrder_main32 input').eq(0).attr('checked')&&$('.zbyOrder_main32 input').eq(1).attr('checked')){
