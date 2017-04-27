6c4f89f0d81fee8934f386fe7f43665ca2881b8c
zhangweiyao <zhangweiyao@bus365.com>
2017-04-27 18:27:48 +0800

    已修改        model/product_detail.model.php

--- model/product_detail.model.php
+++ model/product_detail.model.php
@@ -1,4 +1,5 @@
 <?
+echo "123";
 /// 首页标识
 $is_goods_detail = true;
 
@@ -6,8 +7,8 @@ $is_goods_detail = true;
 $c_goods_id = req('id');
 
 // 产品详细信息
-$sql = "SELECT * FROM t_goods_thread WHERE goods_id='$c_goods_id' AND site_id='$g_siteid' LIMIT 0,1";  
-$c_goods = $db->get_one($sql); 
+$sql = "SELECT * FROM t_goods_thread WHERE goods_id='$c_goods_id' AND site_id='$g_siteid' LIMIT 0,1";
+$c_goods = $db->get_one($sql);
 
 // 产品类型
 $c_goods_type = $c_goods['goods_type'];
@@ -29,67 +30,67 @@ $c_goods_image_list = $db->get_all($sql);
 
 // 评论列表
 function get_comment_list(){
-	global $db, $g_siteid, $c_goods_id;
+    global $db, $g_siteid, $c_goods_id;
 
-	$sql = "SELECT a.*, b.account, b.username, b.nickname FROM t_goods_comment a, t_user b WHERE a.user_id=b.user_id AND a.site_id='$g_siteid' AND a.goods_id='$c_goods_id' AND a.is_first='1' ORDER BY a.comment_id DESC ";
-	return $db->get_all($sql);
+    $sql = "SELECT a.*, b.account, b.username, b.nickname FROM t_goods_comment a, t_user b WHERE a.user_id=b.user_id AND a.site_id='$g_siteid' AND a.goods_id='$c_goods_id' AND a.is_first='1' ORDER BY a.comment_id DESC ";
+    return $db->get_all($sql);
 }
 
 // 平均评分
 function get_avg_comment_score(){
-	global $db, $g_siteid, $c_goods_id;
+    global $db, $g_siteid, $c_goods_id;
 
-	$sql = "SELECT AVG(`comment_star`) FROM t_goods_comment a WHERE a.site_id='$g_siteid' AND a.goods_id='$c_goods_id' AND a.is_first='1' ";
+    $sql = "SELECT AVG(`comment_star`) FROM t_goods_comment a WHERE a.site_id='$g_siteid' AND a.goods_id='$c_goods_id' AND a.is_first='1' ";
 
-	$rs = $db->get_value($sql);
-	$rs = number_format($rs, 1);
-	return $rs;
+    $rs = $db->get_value($sql);
+    $rs = number_format($rs, 1);
+    return $rs;
 }
 
 // 评论总数
 function get_total_comment($level=''){
-	global $db, $g_siteid, $c_goods_id;
+    global $db, $g_siteid, $c_goods_id;
 
-	if($level!=''){
-		$qer = "AND a.`comment_level`='$level' ";
-	}
-	$sql = "SELECT COUNT(*) FROM t_goods_comment a WHERE a.site_id='$g_siteid' AND a.goods_id='$c_goods_id' AND a.is_first='1' $qer ";
-	$rs = $db->get_value($sql);
+    if($level!=''){
+        $qer = "AND a.`comment_level`='$level' ";
+    }
+    $sql = "SELECT COUNT(*) FROM t_goods_comment a WHERE a.site_id='$g_siteid' AND a.goods_id='$c_goods_id' AND a.is_first='1' $qer ";
+    $rs = $db->get_value($sql);
 
-	if($rs=='') {
-		$rs = 0;
-	}
-	return $rs;
+    if($rs=='') {
+        $rs = 0;
+    }
+    return $rs;
 }
 
 // 评论列表
 $comment_list = get_comment_list();
 
 if(notnull($comment_list)){
-	$stat_comment_total = get_total_comment(); //评论总数
+    $stat_comment_total = get_total_comment(); //评论总数
 
-	if($stat_comment_total>0){
+    if($stat_comment_total>0){
 
-		$comment_a_rate = get_total_comment('A') / get_total_comment() * 100;
-		$comment_b_rate = get_total_comment('B') / get_total_comment() * 100;
-		$comment_c_rate = get_total_comment('C') / get_total_comment() * 100;
+        $comment_a_rate = get_total_comment('A') / get_total_comment() * 100;
+        $comment_b_rate = get_total_comment('B') / get_total_comment() * 100;
+        $comment_c_rate = get_total_comment('C') / get_total_comment() * 100;
 
-		$avg_comment_score = get_avg_comment_score(); //平均分
-	}
+        $avg_comment_score = get_avg_comment_score(); //平均分
+    }
 }
 
 
 // 订单总数
 if($g_misc['show_rand_number']=='1'){  //模拟人数
-	 $range = explode(',', $g_misc['rand_number_range']);
-	 $show_rand_number = mt_rand($range[0], $range[1]);
-	 $stat_order_total = $show_rand_number;
+    $range = explode(',', $g_misc['rand_number_range']);
+    $show_rand_number = mt_rand($range[0], $range[1]);
+    $stat_order_total = $show_rand_number;
 } else {
-	$sql = "SELECT SUM(`adult_num`)+SUM(`kid_num`) FROM t_user_order WHERE site_id='$g_siteid' AND goods_id='$c_goods_id' ";
-	$stat_order_total = $db->get_value($sql);
-	if($stat_order_total=='') {
-		$stat_order_total = 0;
-	}
+    $sql = "SELECT SUM(`adult_num`)+SUM(`kid_num`) FROM t_user_order WHERE site_id='$g_siteid' AND goods_id='$c_goods_id' ";
+    $stat_order_total = $db->get_value($sql);
+    if($stat_order_total=='') {
+        $stat_order_total = 0;
+    }
 }
 
 
@@ -100,18 +101,18 @@ if($g_misc['show_rand_number']=='1'){  //模拟人数
 $this_parent_catalog = array();
 
 function list_parent_catalog($this_catalog_id){
-	global $db, $g_siteid, $this_parent_catalog;
+    global $db, $g_siteid, $this_parent_catalog;
 
-	$sql = "SELECT * FROM `t_goods_catalog` WHERE `cat_id`='$this_catalog_id' AND `site_id`='$g_siteid'";
-	$this_row = $db->get_one($sql);
+    $sql = "SELECT * FROM `t_goods_catalog` WHERE `cat_id`='$this_catalog_id' AND `site_id`='$g_siteid'";
+    $this_row = $db->get_one($sql);
 
-	if($this_row['cat_id']!=''){
-		$this_parent_catalog[] = $this_row;
-	}
+    if($this_row['cat_id']!=''){
+        $this_parent_catalog[] = $this_row;
+    }
 
-	if($this_row['parent_id']!='' && $this_row['parent_id']!='0'){
-		list_parent_catalog($this_row['parent_id']);
-	}
+    if($this_row['parent_id']!='' && $this_row['parent_id']!='0'){
+        list_parent_catalog($this_row['parent_id']);
+    }
 }
 
 // 执行遍历所有父类
@@ -126,21 +127,21 @@ $c_shop_name = $c_shop['shop_name'];
 $c_shop_url  = 'http://'.$c_shop['shop_domain'].'.'.$g_shop_root_domain;
 
 if($c_shop_name == ''){
-	$c_shop_name = $g_profile['company'].'(自营)';
-	$c_shop_url  = 'javascript:void(0)';
+    $c_shop_name = $g_profile['company'].'(自营)';
+    $c_shop_url  = 'javascript:void(0)';
 }
 
 
 $sql = "SELECT *, DATE_FORMAT(`departdate`,'%Y%m%d') AS ymd FROM `t_goods_sku` WHERE `site_id`='$g_siteid' AND `goods_id`='$c_goods_id' AND `departdate`>='".date('Y-m-d')."'";
 $c_goods_sku = $db->get_all($sql);
 if(notnull($c_goods_sku)){
-	foreach ($c_goods_sku as $sval){
-		$adult_price_array[$sval['ymd']] = $sval['adult_price'];
-		$adult_stock_array[$sval['ymd']] = $sval['adult_stock'];
-		$kid_price_array[$sval['ymd']]   = $sval['kid_price'];
-		$kid_stock_array[$sval['ymd']]   = $sval['kid_stock'];
-		$diff_price_array[$sval['ymd']]  = $sval['diff_price'];
-	}
+    foreach ($c_goods_sku as $sval){
+        $adult_price_array[$sval['ymd']] = $sval['adult_price'];
+        $adult_stock_array[$sval['ymd']] = $sval['adult_stock'];
+        $kid_price_array[$sval['ymd']]   = $sval['kid_price'];
+        $kid_stock_array[$sval['ymd']]   = $sval['kid_stock'];
+        $diff_price_array[$sval['ymd']]  = $sval['diff_price'];
+    }
 }
 
 /// 价格体系
@@ -157,20 +158,23 @@ $all_contents	= $all_days['content'];
 $all_tools		= $all_days['tool'];
 $all_images		= $all_days['image'];
 
-$goodsId = req('goodsId');
-$productId = req('productId');
-$url = $host . "/travel/interface/zby/v3.2/getZbyGoodsDtail_v3.2?goodsId=" . $goodsId;
+$goodsId = req('id');
+$url = $host . "/travel/interface/zbyV3.2/getZbyGoodsDtailV_3.2?goodsId=" . $goodsId;
 $rst = $db->api_post($url);
 $arr = json_decode($rst, true);
-$data = $arr['data'];   
-$scheduling = $data['scheduling'];
+$data = $arr['data'];
+
+//var_dump($data['imageList']);
+//echo "<pre>";
+//var_dump($arr);
+//die;
 
 function seo(){
-	global $g_sitename, $c_goods;
-?>
-<title><?=$c_goods['goods_name']?>_<?=$g_sitename?></title>
-<meta name="keywords" content="<?=$c_goods['goods_name']?>" />
-<meta name="description" content="<?=$c_goods['goods_name']?> <?=str_replace("\n","",removehtml($c_goods['summary']))?> " />
-<?
+    global $g_sitename, $c_goods;
+    ?>
+    <title><?=$c_goods['goods_name']?>_<?=$g_sitename?></title>
+    <meta name="keywords" content="<?=$c_goods['goods_name']?>" />
+    <meta name="description" content="<?=$c_goods['goods_name']?> <?=str_replace("\n","",removehtml($c_goods['summary']))?> " />
+    <?
 }
 ?>
