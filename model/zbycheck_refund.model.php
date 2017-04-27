a526f4da074d61b03a93e414aeed638720a7a8e4
heyanliang <heyanliang@bus365.com>
2017-04-27 11:28:15 +0800

    已修改        model/zbycheck_refund.model.php 已修改        model/zbyorder_detail.model.php 已修改        themes/s01/zbyorder_detail.php 已修改        themes/s01/zbyrefund.php 周边游退款成功

--- model/zbycheck_refund.model.php
+++ model/zbycheck_refund.model.php
@@ -4,17 +4,14 @@ $orderCode =$_POST['orderCode'];
 $token = substr($_COOKIE['5fe845d7c136951446ff6a80b8144467'],1,-1);
 //退款产品信息检验
 $post1 = array('orderCode' => $orderCode, 'token' => $token);
-//$refund_product = $db->api_post($host . "/travel/interface/zby/zbyRefundInfo", $post1);
 $refund_product = juhecurl( $host . "/travel/interface/zby/zbyRefundInfo", $post1, 1);
 $refund_product = json_decode($refund_product, true);
-//$refund_product = array_iconv($refund_product);
 $refund_product_data = $refund_product['data'];
 $refundReasonList = $refund_product_data['refundReasonList'];
 $isChange = $refund_product_data['isChange'];
 $failReason = $refund_product_data['failReason'];
-//echo $refund_product_data['isChange'];
-//echo 'dsfsf';
-var_dump($refund_product);
+echo $isChange;
+//var_dump($refund_product);
 
 
 
