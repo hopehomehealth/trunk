3a99a8bec5540a8e6497f0463a8c100859968cf2
zhangweiyao <zhangweiyao@bus365.com>
2017-04-27 14:18:10 +0800

    已修改        member/ajax.price.php

--- member/ajax.price.php
+++ member/ajax.price.php
@@ -8,15 +8,8 @@ $post['roomMax'] = $_POST['roomMax'];
 $post['goodsType'] = $_POST['goodsType'];
 $post['isPackage'] = $_POST['isPackage'];
 $diffPrice = $_POST['diffPrice'];
-$url = "wwwd.bus365.cn" . "/travel/interface/zby/v3.2/getDiffRoomNum_v3.2";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-$data = curl_exec($ch);
-curl_close($ch);
+$url = $host . "/travel/interface/zby/v3.2/getDiffRoomNum_v3.2";
+$data = $db->api_post($url, $post);
 $arr = json_decode($data, true);
 $fangcha = $arr['data'];
 
