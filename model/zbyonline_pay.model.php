902a30134864e36a6f736d76f5228fbf3b08ab02
heyanliang <heyanliang@bus365.com>
2017-05-02 20:53:28 +0800

    已修改        model/zbyonline_pay.model.php

--- model/zbyonline_pay.model.php
+++ model/zbyonline_pay.model.php
@@ -13,9 +13,10 @@ function get_pay_way()
     $pay_way = juhecurl($host . "/travel/interface/pay/getPayWays", $trans, 1);
     $pay_way = json_decode($pay_way, true);
     $pay_way = array_iconv($pay_way, 'UTF-8', 'GBK');
-//    if ($pay_way['status'] != '0000'){
-//        exit('获取参数失败');
-//    }
+//    var_dump($pay_way);
+    if ($pay_way['status'] != '0000'){
+        exit($pay_way['msg']);
+    }
     return $pay_way;
 }
 $pay_way = get_pay_way();
