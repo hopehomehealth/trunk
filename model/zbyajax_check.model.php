8d07d402c297385e6b14f4d4c2e8f8d1b15f7e63
heyanliang <heyanliang@bus365.com>
2017-05-02 17:37:26 +0800

    已修改        model/zbyajax_check.model.php

--- model/zbyajax_check.model.php
+++ model/zbyajax_check.model.php
@@ -1,10 +1,10 @@
 <?
 include '../config.php';
 $orderCode =$_POST['orderCode'];
-//$flag = $_POST['flag'];
+$flag = $_POST['flag'];
 $token = substr($_COOKIE['5fe845d7c136951446ff6a80b8144467'],1,-1);
 //退款产品信息检验
-//if(!empty($orderCode) && $flag == 'chk') {
+if(!empty($orderCode) && $flag == 'chk') {
     $post1 = array('orderCode' => $orderCode, 'token' => $token);
     $refund_product = juhecurl($host . "/travel/interface/zby/v3.2/zbyRefundInfo_v3.2", $post1, 1);
     $refund_product = json_decode($refund_product, true);
@@ -14,5 +14,5 @@ $token = substr($_COOKIE['5fe845d7c136951446ff6a80b8144467'],1,-1);
     $failReason = $refund_product_data['failReason'];
     echo $isChange;
 //var_dump($refund_product);
-//}
+}
 
