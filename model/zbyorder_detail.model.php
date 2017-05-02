6d41d0b1abc1d8e6c4c4f078466600505950f994
heyanliang <heyanliang@bus365.com>
2017-05-02 21:03:53 +0800

    已修改        model/zbyorder_detail.model.php

--- model/zbyorder_detail.model.php
+++ model/zbyorder_detail.model.php
@@ -9,13 +9,14 @@ $post1 = array('orderCode' => $orderCode, 'token' => $token);
 $refund_product = juhecurl($host . "/travel/interface/zby/v3.2/zbyRefundInfo_v3.2", $post1, 1);
 $refund_product = json_decode($refund_product, true);
 $refund_product = array_iconv($refund_product);
+if ($refund_product['status'] != '0000') {
+    exit($refund_product['msg']);
+}
 $refund_product_data = $refund_product['data'];
 $refundReasonList = $refund_product_data['refundReasonList'];
 $isChange = $refund_product_data['isChange'];
 $failReason = $refund_product_data['failReason'];
-if ($refund_product['status'] != '0000') {
-    exit($refund_product['msg']);
-}
+
 //echo "<pre>";
 //var_dump($refund_product_data);
 //退款申请
@@ -24,11 +25,12 @@ if(req('flag') == 'rf'){
     $require_refund = $db->api_post($host."/travel/interface/zby/v3.2/refundZby_v3.2",$post2);
     $require_refund = json_decode($require_refund, true);
     $require_refund = array_iconv($require_refund);
-    $require_refund_data = $require_refund['data'];
-    $refund_message = $require_refund_data['refundCustomerInfo'];
     if ($require_refund['status'] != '0000') {
         exit($require_refund['msg']);
     }
+    $require_refund_data = $require_refund['data'];
+    $refund_message = $require_refund_data['refundCustomerInfo'];
+
 ?>
 <form action="<?=$g_self_domain?>/zhoubianyou/zbyrefund-<?=$orderCode;?>.html" method="post" id="refundForm">
     <input type="hidden" name="message" id="message" value="<?=$refund_message;?>">
@@ -46,10 +48,11 @@ if(req('flag') == 'cf'){
     $confirm_return = juhecurl($host."/travel/interface/zby/confirmBackGroup",$post3, 1);
     $confirm_return = json_decode($confirm_return, true);
     $confirm_return = array_iconv($confirm_return);
-    $confirm_return_data = $confirm_return['data'];
     if ($confirm_return['status'] != '0000') {
         exit($confirm_return['msg']);
     }
+    $confirm_return_data = $confirm_return['data'];
+
 }
 
 //订单详情展示
@@ -57,21 +60,23 @@ $post4 = array('orderCode' => $orderCode, 'token' => $token);
 $order_detail = juhecurl($host . "/travel/interface/zby/v3.2/getZbyOrderDtail_v3.2", $post4, 1);
 $order_detail = json_decode($order_detail, true);
 $order_detail = array_iconv($order_detail);
-$order_detail_data = $order_detail['data'];
-$orderStatus = $order_detail_data['orderStatus'];
 if ($order_detail['status'] != '0000') {
     exit($order_detail['msg']);
 }
+$order_detail_data = $order_detail['data'];
+$orderStatus = $order_detail_data['orderStatus'];
+
 //取消订单
 if($_GET['flag'] == 'cn'){
     $post5 = array('orderCode' => $orderCode, 'token' => $token);
     $cancle_order = juhecurl($host."/travel/interface/order/cancleUserOrder",$post5, 1);
     $cancle_order = json_decode($cancle_order, true);
     $cancle_order = array_iconv($cancle_order);
-    $cancle_order_data = $cancle_order['data'];
     if ($cancle_order['status'] != '0000') {
         exit($cancle_order['msg']);
     }
+    $cancle_order_data = $cancle_order['data'];
+
 }
 
 
