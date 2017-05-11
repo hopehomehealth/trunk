af1ced674d057bfe8b1d1b36614af14065a59ac5
heyanliang <heyanliang@bus365.com>
2017-05-11 14:02:40 +0800

    已修改        themes/s01/product_list.php

--- themes/s01/product_list.php
+++ themes/s01/product_list.php
@@ -541,7 +541,7 @@
                             } else {
                                 echo $ziyingurl;
                             } ?>" target="_blank" class="btn  btn-sm">查看详情</a>
-                            <div class="count"> </a></div>
+<!--                            <div class="count"> </a></div>-->
                             <?if($val['aLiData'] == '飞猪自营'){ ?><p style="text-align: center;color: #999;line-height: 28px;font-size: 12px;font-weight: bold;">本服务由飞猪提供</p><? } ?>
                         </div>
                     </div>
