9cc458e27cfdb0055f28190a42b3e9e8543eec68
zhangweiyao <zhangweiyao@bus365.com>
2017-05-12 09:55:46 +0800

    已修改        model/check_meal.model.php

--- model/check_meal.model.php
+++ model/check_meal.model.php
@@ -7,16 +7,16 @@ $post['lvProductId'] = $_POST['lvProductId'];
 $post['departDate'] = $_POST['departDate'];
 $post['adultNum'] = $_POST['adultNum'];
 $post['childNum'] = $_POST['childNum'];
-$url =  $host . "/travel/interface/zby/v3.2/checkDataBeforePay";
+$url =   "192.168.0.223:8080/travel/interface/zby/v3.2/checkDataBeforePay";
 $data = $db->api_post($url, $post);
 $arr = json_decode($data, true);
 $datas = $arr['data'];
-if($datas['timeMsg'] !== '1'  ){
-    echo $db->to_gbk($datas['timeMsg']);
+if($datas['skuMsg'] !== '1'){
+    echo $db->to_gbk($datas['skuMsg']);
 } else if ($datas['dayMsg'] !== '1'){
     echo $db->to_gbk($datas['dayMsg']);
-} else if ($datas['skuMsg'] !== '1'){
-    echo $db->to_gbk($datas['skuMsg']);
+} else if ($datas['timeMsg'] !== '1'){
+    echo $db->to_gbk($datas['timeMsg']);
 } else {
     echo "1";
 }
