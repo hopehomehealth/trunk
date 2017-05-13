27077f0e04c2435c9b5ddcb7fa20e9ba8e7928dd
heyanliang <heyanliang@bus365.com>
2017-05-13 15:19:49 +0800

    已修改        themes/s01/index.php 已修改        themes/s01/product_list.php

--- themes/s01/product_list.php
+++ themes/s01/product_list.php
@@ -422,11 +422,12 @@
 //                echo "<pre>";
 //                var_dump($val);
                 $goodsImage = $val['goodsImage'];
+//                echo $goodsImage;
                 $sku_list = get_sku_list($val['goodsId'], 5);
                 $goods_url = $val['pcALiDetailLink'];
                 $goodsId = $val['goodsId'];
                 $productId = $val['productId'];
-                $ziyingurl = "/product/detail-" . $goodsId . "-" . $productId . ".html"
+                $ziyingurl = "/product/detail-" . $goodsId . "-" . $productId . ".html";
                 ?>
                 <div class="lv-list">
                     <!-- 产品信息 -->
